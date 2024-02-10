924a56c9ac395a7bd21ca50b93ab382c67c7ddf8
damarisdwayne <damaris.dwayne@pensiondynamics.com.br>
2024-02-10 08:08:00 -0300

    feat: modal created

--- src/app.tsx
+++ src/app.tsx
@@ -5,7 +5,7 @@ import { NoteCard } from "./components/note-card";
 const App = () => {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
-      <img src={logo} alt="" />
+      <img src={logo} alt="NLW Expert" className="h-7" />
       <form>
         <input
           type="text"
@@ -18,8 +18,7 @@ const App = () => {
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={{ date: new Date(), content: "teste" }} />
       </div>
     </div>
   );
